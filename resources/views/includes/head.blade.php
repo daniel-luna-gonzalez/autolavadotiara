d150bd9ba7d25ad29afeab7ed0f94f84b3aee87e
daniel.lunagonzalez91@gmail.com <Dan13llg>
2018-10-26 17:36:17 -0500

    remove unnecessary files

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -7,34 +7,29 @@
 <meta property="og:type" content="website" />
 <meta property="og:title" content="Tiara Autolavado!" />
 <meta property="og:description" content="tiaraautolavado.com" />
-<meta property="og:image" content="{{ env('APP_URL') }}/media/images/hazlorealidad-share-fb.png" />
 <link rel="stylesheet" href="apis/bootstrap/css/bootstrap.css">
 <link rel="stylesheet" type="text/css" href="apis/bootstrap-toggle/css/bootstrap-toggle.css" />
 <link rel="stylesheet" href="apis/bootstrap3-dialog/css/bootstrap-dialog.css"/>
 <link rel="stylesheet" href="apis/bootstrap-datetimepicker/css/bootstrap-datetimepicker.min.css"/>
 <!--<link rel="stylesheet" type="text/css" href="apis/pagepiling/jquery.pagepiling.css" />-->
-<link rel="stylesheet" href="css/home.css">
+{{--<link rel="stylesheet" href="css/home.css">--}}
 <link rel="stylesheet" href="css/hazlorealidad.css">
 <link rel="stylesheet" href="css/navbar.css">
-<link rel="stylesheet" href="css/quienes_somos.css">
+{{--<link rel="stylesheet" href="css/quienes_somos.css">--}}
 <link rel="stylesheet" href="css/SeleccionTipoAuto.css">
 <link rel="stylesheet" href="css/paquetes.css">
-<link rel="stylesheet" href="css/donor-causes.css">
+{{--<link rel="stylesheet" href="css/donor-causes.css">--}}
 <link rel="stylesheet" href="css/contacto.css">
-<link rel="stylesheet" href="css/preguntas.css">
-<link rel="stylesheet" href="css/porquedonar.css">
-<link rel="stylesheet" href="css/causas.css">
-<link rel="stylesheet" href="css/testimoniales.css">
-<link rel="stylesheet" href="css/donadores.css">
+{{--<link rel="stylesheet" href="css/preguntas.css">--}}
 
 <link rel="stylesheet" type="text/css" href="apis/paymentInfo/assets/css/normalize.css">
 <link rel="stylesheet" type="text/css" href="apis/paymentInfo/assets/css/paymentInfo.css">
-<link rel="stylesheet" type="text/css" href="apis/jquery.bxslider/jquery.bxslider.css">
+{{--<link rel="stylesheet" type="text/css" href="apis/jquery.bxslider/jquery.bxslider.css">--}}
 <link rel="apple-touch-icon" href="apis/paymentInfoassets/apple-touch-icon.png">
 <link rel="stylesheet" href="apis/scrolling-nav/css/scrolling-nav.css">
 <link rel="stylesheet" href="apis/jquery-ui/jquery-ui.css">
-<link href="//vjs.zencdn.net/5.11/video-js.min.css" rel="stylesheet">
-<script src="//vjs.zencdn.net/5.11/video.min.js"></script>
+{{--<link href="//vjs.zencdn.net/5.11/video-js.min.css" rel="stylesheet">--}}
+{{--<script src="//vjs.zencdn.net/5.11/video.min.js"></script>--}}
 <script data-main="js/main" src="apis/require.js"></script>
 
 <!-- google analitycs -->
